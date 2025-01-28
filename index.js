12c22c4e0aba9c57716f433804bf7d40d4678179
eeemessi11 <benanmachadi@gmail.com>
2025-01-28 10:22:50 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const sharp = require("sharp")
+const axios = require("axios")
+const os = require("os");
+const path = require("path");
+
+async function SaveToPath(PATH, IMAGE_URL) {
+    try {
+      const response = await axios({
+        url: IMAGE_URL,
+        responseType: 'arraybuffer',
+      });
+      await sharp(response.data)
+        .toFile(PATH);
+  
+    } catch (err) {
+      throw new Error(err)
+    }
+}
+
+async function GetImageFromSubreddit(SUBREDDIT, WHERE_TO_STORE, RETURN_URL) {
+    if (!SUBREDDIT) {throw new Error("You didnt specify the subreddit!");}
+    try {
+        var LINK = "https://www.reddit.com/r/" + SUBREDDIT + "/.json"
+        var RES = await fetch(LINK)
+        var DATA = await RES.json()
+        var _ = DATA.data.children
+        var POSTS = _.filter(POST => POST.data.thumbnail && POST.data.thumbnail.startsWith("http")) || {}
+        if (POSTS.length == 0) {throw new Error("There are 0 posts with images")}
+        var POST = POSTS[Math.floor(Math.random() * POSTS.length)]
+        var POST_DATA = POST.data
+        var IMAGE_URL = POST_DATA.thumbnail
+        if (RETURN_URL) {
+            return IMAGE_URL
+        } else {
+            SaveToPath(WHERE_TO_STORE, IMAGE_URL)
+        }
+    } catch (ERR) {
+        throw new Error(ERR); 
+    }
+}
+
+module.exports = GetImageFromSubreddit
